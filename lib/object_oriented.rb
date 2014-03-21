401b0631d567ed84c0e1fbb680afc776122de56a
Ionova Natalya <ionovatata@yandex.ru>
2014-03-21 11:42:51 +0400

    Добавила объектно-ориентированную версию

---
+++ lib/object_oriented.rb
@@ -0,0 +1,34 @@
+# encoding: utf-8
+
+class Person
+
+  def initialize(first_name, last_name, given_name)
+    @first_name = first_name
+    @last_name = last_name
+    @given_name = given_name
+  end
+
+  def full_name
+    @last_name + " " + @first_name + " " + @given_name
+  end
+
+  def short_name
+    @first_name + " " + @last_name
+  end
+
+end
+
+a = Person.new("Антон", "Агеев", "Юрьевич")
+puts a.full_name
+puts a.short_name
+puts
+
+k = Person.new("Катерина", "Назарова", "Андреевна")
+puts k.full_name
+puts k.short_name
+puts
+
+i = Person.new("Наталья", "Ионова", "Алексеевна")
+puts i.full_name
+puts i.short_name
+puts
